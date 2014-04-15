d55e0a62eaf3d18aa13b8564398677703adcafcf
djinvisible <djinvisible_jsp@hotmail.com>
2014-04-15 13:36:38 -0400

    Create PokerGame.java

---
+++ PokerGame.java
@@ -0,0 +1,18 @@
+package peterson;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class PokerGame {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		
+	ReadIn run = new ReadIn();
+		
+	}
+
+}
