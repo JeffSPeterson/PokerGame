b26e0a97e3c510315e993d53335b91cda145b064
djinvisible <djinvisible_jsp@hotmail.com>
2014-04-15 15:40:42 -0400

    Update ReadIn.java

--- ReadIn.java
+++ ReadIn.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
 
 public class ReadIn {
 
-	public final static int ACE = 1;
 	public final static int TEN = 10;
 	public final static int JACK = 11;
 	public final static int QUEEN = 12;
 	public final static int KING = 13;
+	public final static int ACE = 14;
 	public final static int SPADE = 20;
 	public final static int HEART = 21;
 	public final static int DIAMOND = 22;
@@ -18,20 +18,20 @@ public class ReadIn {
 
 	Scanner scan = new Scanner(new File("hands.txt"));
 	String str;
-	int d, s, count = 0;
-	Card[] cards = new Card[5];
+	int d, s, playerOneWins, playerTwoWins, count = 0;
 	Card card;
-	Hand p1Hand;
-	Hand p2Hand;
+	Card[] hand1 = new Card[5];
+	Card[] hand2 = new Card[5];
+	CheckHand check = new CheckHand();
 
 	public ReadIn() throws IOException {
 
-		// ////////READ IN FILE//////////////////////////
+		// READ IN FILE
 		while (scan.hasNext()) {
 
 			str = scan.next();
-			// ///////// IF 10, J, K, Q, A TURN INTO
-			// INTO/////////////////////////
+
+			// IF 10, J, K, Q, A.....
 			if (str.substring(0, 1).equals("T"))
 				d = TEN;
 			else if (str.substring(0, 1).equals("J"))
@@ -45,7 +45,7 @@ public class ReadIn {
 			else
 				d = Integer.parseInt(str.substring(0, 1));
 
-			// ///////// TURN SUIT INTO INT ///////////////
+			// TURNS SUIT INTO INT
 			if (str.substring(1).equals("S"))
 				s = SPADE;
 			else if (str.substring(1).equals("H"))
@@ -55,12 +55,39 @@ public class ReadIn {
 			else
 				s = CLUB;
 
-			// NEW CARD FROM INPUT
+			// NEW CARD FROM DIGIT:SUIT INPUT
 			card = new Card(d, s);
-			
-			System.out.println(card.digit + " " + card.suit);
-		
+
+			// SEPARATE CARDS INTO TWO HANDS
+			if (count < 5) {
+				hand1[count] = card;
+			} else if (count >= 5 && count < 10) {
+				hand2[count % 5] = card;
+			}
+			count++;
+
+			// RESET COUNT TO 0 TO GET NEXT SET OF HANDS
+			if (count == 10) {
+				// CHECK HAND TO SEE WHO WON
+				if (check.checkHand(hand1) > check.checkHand(hand2))
+					playerOneWins++;
+				else
+					playerTwoWins++;
+
+				count = 0;
+			}
 		}
+
+		// OUTPUT OF WINNER
+		System.out.println("Player One Has: " + playerOneWins + "\n"
+				+ "Player Two Has: " + playerTwoWins);
+		
+		if (playerOneWins > playerTwoWins)
+			System.out.println("Player One Wins!!!");
+		else
+			System.out.println("Player Two Wins!!!");
+
+		// CLOSE SCANNER
 		scan.close();
 	}
 }
